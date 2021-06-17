fd3764fc39dc243bf9fb1da0f6641bc414b1dab7
vedashree943 <77153920+vedashree943@users.noreply.github.com>
2021-06-17 15:32:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function sun() {
+    document.getElementById("sun").style.visibility="visible";
+    
+}
+function mars() {
+    document.getElementById("sun").innerHTML=" Mars Is The Fourth Planet From The Sun."+"<br>"+"The Distance From The Sun Is 227.9 Million Km."+"<BR>"+"The Length Of The Day Is 1 Day 37 minutes.";
+    
+}
+function earth() {
+    document.getElementById("sun").innerHTML="Earth is the third planet from the Sun and the only astronomical object known to harbor and support life."+"<br>"+"Earth's atmosphere consists mostly of nitrogen and oxygen."+"<BR>"+" About 29.2% of Earth's surface is land consisting of continents and islands.";
+    
+}
+function mercury() {
+    document.getElementById("sun").innerHTML="Mercury is a rocky planet, also known as a terrestrial planet." +"<br>"+"Mercury has a solid, cratered surface, much like the Earth's moon."+"<br>"+"Distance from Sun: 57.91 million km";
+    
+}
+function venus() {
+    document.getElementById("sun").innerHTML="Venus is the second planet from the Sun."+ "<br>"+" It is named after the Roman goddess of love and beauty."+" <br> "+"Distance from Sun: 108.2 million km";
+    
+}
+function jupiter() {
+    document.getElementById("sun").innerHTML="Jupiter is the fifth planet from the Sun and the largest in the Solar System."+ "<br>"+" Jupiter is the fifth planet from the Sun and the largest in the Solar System."+"<br>"+ "Distance from Sun: 778.5 million km.";
+    
+}
+function saturn() {
+    document.getElementById("sun").innerHTML="Saturn isn't the only planet to have rings, but it definitely has the most beautiful ones."+"<br>"+"Saturn is the sixth planet from the Sun."+"<br>"+"It only has one-eighth the average density of Earth.";
+    
+}
+function uranus() {
+    document.getElementById("sun").innerHTML="Uranus is the seventh planet from the Sun."+"<br>"+"Its name is a reference to the Greek god of the sky, Uranus, who, according to Greek mythology."+"<br>"+"It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.";
+    
+}
+function neptune() {
+    document.getElementById("sun").innerHTML="Neptune is the eighth and farthest-known Solar planet from the Sun."+"<br>"+"the Solar System, it is the fourth-largest planet by diameter"+"<br>"+"It is 17 times the mass of Earth, slightly more massive than its near-twin Uranus";
+    
+}
